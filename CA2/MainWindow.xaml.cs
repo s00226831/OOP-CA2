4aa08b1ab5d020374771286243f7745382daecf9
MeganBC <123363704+s00226831@users.noreply.github.com>
2023-12-15 01:30:15 +0000

    GUI Created

---
+++ CA2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CA2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        // here is a link to the github page: https://github.com/s00226831/OOP-CA2
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLoss_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDraw_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
