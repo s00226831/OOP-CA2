c93da1d27fd81ba424eec95b8e61f7fa63db38b4
MeganBC <123363704+s00226831@users.noreply.github.com>
2023-12-15 02:15:48 +0000

    Classes made

---
+++ CA2/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    internal class Player
+    {
+        // name property 
+        public string Name
+        {
+            get; 
+            set;
+        }
+        // property to record results
+        public string ResultRecords
+        {
+            get;
+            set;
+        }
+
+        // contructor to create a player
+        public Player(string name, string resultRecords)
+        {
+            Name = name;
+            ResultRecords = resultRecords;
+        }
+    }
+}
---
+++ CA2/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    internal class Team
+    {
+        // team name property
+        public string Name 
+        { 
+            get; 
+            set; 
+        }
+        // list of players
+        public List<Player> Players
+        {
+            get;
+            set;
+        }
+
+        // contructor to make a team and list the players in a team
+        public Team ()
+        {
+            Players = new List<Player> ();
+        }
+    }
+}
